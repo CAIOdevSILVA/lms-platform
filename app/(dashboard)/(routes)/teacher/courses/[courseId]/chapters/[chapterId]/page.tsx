f00b371b902f829785cead938e302e7502691ec8
Caio Silva <caiossantosp5560@gmail.com>
2023-10-13 15:26:47 -0300

    feat: adicionar funcionalidade para deletar e publicar um capitulo

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,13 +1,16 @@
-import IconBadge from '@/components/IconBadge';
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { ArrowLeft, Eye, LayoutDashboard, Video } from 'lucide-react';
-import Link from 'next/link';
 import { redirect } from 'next/navigation';
+
+import Link from 'next/link';
+import IconBadge from '@/components/IconBadge';
 import ChapterTitleForm from './_components/ChapterTitleForm';
 import ChapterDescriptionForm from './_components/ChapterDescriptionForm';
 import ChapterAccessForm from './_components/ChapterAccessForm';
 import ChapterVideoForm from './_components/ChapterVideoForm';
+import Banner from '@/components/Banner';
+import ChapterActions from './_components/ChapterActions';
 
 const ChapterIdPage = async (
 	{ params } :
@@ -44,8 +47,18 @@ const ChapterIdPage = async (
 
 		const completionText = `${completedFields}/${totalFields}`;
 
+		const isComplete = requiredFields.every(Boolean);
+
 	return (
 		<>
+			{!chapter?.isPublished && (
+				<>
+					<Banner
+						label='This chapter is unpublished. It will not be visible in the course'
+						variant={'warning'}
+					/>
+				</>
+			)}
 			<div className='p-6'>
 				<div className='flex items-center justify-between'>
 					<div className='w-full'>
@@ -61,8 +74,16 @@ const ChapterIdPage = async (
 						<div className='flex items-center justify-between w-full'>
 							<div className='flex flex-col gap-y-2'>
 								<h1 className='text-xl font-medium'>Chapter Creation</h1>
-								<span className='text-sm text-slate-700'>Complete all fields {completionText}</span>
+								<span className='text-sm text-slate-700'>
+									Complete all fields {completionText}
+								</span>
 							</div>
+							<ChapterActions
+								disabled={!isComplete}
+								courseId={params.courseId}
+								chapterId={params.chapterId}
+								isPublished={chapter?.isPublished}
+							/>
 						</div>
 					</div>
 				</div>
